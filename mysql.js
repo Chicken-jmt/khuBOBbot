b98e9314daa79d364ba6c0d66fde4f1232b7b14d
EC2 Default User <ec2-user@ip-172-31-0-217.ec2.internal>
2022-12-19 08:43:26 +0000

    Udpate mysql.js

--- mysql.js
+++ mysql.js
@@ -1,6 +1,6 @@
-var mysql = require('mysql');
-
-const connection = mysql.createConnection({
+// var mysql = require('mysql2/promise');
+const mysql = require('sync-mysql');
+const connection  = new mysql({
     host: "ossw10jo-database.c0e4aijkeltd.us-east-1.rds.amazonaws.com",
     port: 3306,
     user: "ossw10jo",
@@ -8,14 +8,75 @@ const connection = mysql.createConnection({
     database: "ossw10jo",
 });
 
-connection.connect();
 
-connection.query("select * from restaurant", function(err, res, fileds){
-    if(err){
-        console.error(err);
-        return ;
-    }   
+// const pool = mysql.createPool({
+//     host: "ossw10jo-database.c0e4aijkeltd.us-east-1.rds.amazonaws.com",
+//     port: 3306,
+//     user: "ossw10jo",
+//     password: "ossw10jo",
+//     database: "ossw10jo",
+// });
+//
+// async function name() {
+//     const conn = await pool.getConnection(async conn => conn);
+//     const nameSql = "select name from restaurant";
+//     const [sqlName] = await conn.query(nameSql);
+//     //return sqlName;
+//     return (sqlName);
+//     // console.log(sqlName);
+// };
+//
+// async function address() {
+//     const conn = await pool.getConnection(async conn => conn);
+//     const addressSql = "select address from restaurant";
+//     const [sqlAddress] = await conn.query(addressSql);
+//     //return sqlName;
+//     return (sqlAddress);
+//     // console.log(sqlName);
+// }
 
-    console.log(res[0]);
-})
-connection.end();
+// async function category() {
+//     const conn = await pool.getConnection(async conn => conn);
+//     const categorySql = "select category from category";
+//     const [sqlCategory] = await conn.query(categorySql);
+//     //return sqlName;
+//     return (sqlCategory);
+//     // console.log(sqlName);
+// }
+
+// name().
+//     then((name) => {
+//         console.log(name[0].name);
+// });
+//
+// address().
+//     then((address) => {
+//         console.log(address);
+// });
+//
+// category().
+//     then((category) => {
+//         console.log(category);
+// });
+
+
+// module.exports = {
+//     name,
+//     address,
+//     category,
+// };
+// connection.connect();
+//
+let restaurant_table = connection.query('select restaurant.*, category.category from restaurant inner join category on restaurant.seq = category.seq');
+let menu_table = connection.query('select * from menu');
+let schoolfood_table = connection.query('select * from school');
+
+
+module.exports = {
+    restaurant_table,
+    menu_table,
+    schoolfood_table
+}
+//
+//
+// connection.end();
